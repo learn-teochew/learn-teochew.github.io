128d580bf22517a7bd924699da82bf6e808a0062
kbseah <kb.seah@gmail.com>
2024-02-24 21:46:00 +0100

    Format python code with black

--- assets/scripts/zh2pengim.py
+++ assets/scripts/zh2pengim.py
@@ -4,54 +4,70 @@ import json
 import argparse
 import sys
 
+
 def rubify(char, annot):
     """Return ruby annotated html tag
 
     Multiple pronunciations are stacked with breaklines
     within the ruby text
     """
-    return(f"<ruby>{char}<rt>{annot}</rt></ruby>")
+    return f"<ruby>{char}<rt>{annot}</rt></ruby>"
+
 
 # CJK punctuation to Western
 puncdict = {
-    '，' : ',',
-    '。' : '.',
-    '、' : ',',
-    '！' : '!',
-    '？' : '?',
-    '；' : ';',
-    '：' : ':',
-    '「' : '‘',
-    '」' : '’',
-    '【' : '[',
-    '】' : ']',
-    '（' : '(',
-    '）' : ')',
-    '《' : '“',
-    '》' : '”',
-        }
+    "，": ",",
+    "。": ".",
+    "、": ",",
+    "！": "!",
+    "？": "?",
+    "；": ";",
+    "：": ":",
+    "「": "‘",
+    "」": "’",
+    "【": "[",
+    "】": "]",
+    "（": "(",
+    "）": ")",
+    "《": "“",
+    "》": "”",
+}
 
 parser = argparse.ArgumentParser(
-    description="Convert chinese characters to peng'im with dictionary")
+    description="Convert chinese characters to peng'im with dictionary"
+)
 parser.add_argument(
-    "-i", "--input", type=argparse.FileType('r'), nargs='?',
-    default=sys.stdin, action='store',
+    "-i",
+    "--input",
+    type=argparse.FileType("r"),
+    nargs="?",
+    default=sys.stdin,
+    action="store",
     help="""
     File with chinese characters to be converted to peng'im, read from STDIN
     otherwise
-    """)
+    """,
+)
 parser.add_argument(
-    "-d", "--dict", type=str, action='store',
+    "-d",
+    "--dict",
+    type=str,
+    action="store",
     help="""
     JSON file with dictionary, produced by scraping Wiktionary dump with
     wiktionary_teochew_parse.py
-    """)
+    """,
+)
 parser.add_argument(
-    "-f", "--format", default='html', help="""
+    "-f",
+    "--format",
+    default="html",
+    help="""
     Output format: 'html' - html with pengim as ruby, 'replace' - replace hanzi
     with pengim, 'interlinear' - original lines interleaved with pengim
-    """)
-args=parser.parse_args()
+    """,
+)
+args = parser.parse_args()
 
 # Read dictionary
 dd_fh = open(args.dict, "r")
@@ -61,16 +77,16 @@ dd_fh.close()
 # Process input
 
 # Output html
-if args.format == 'html':
+if args.format == "html":
     print("<!DOCTYPE html>\n<html>\n<body>")
     for line in args.input:
         outline = []
         for i in line.rstrip():
             if i in [str(i) for i in range(10)]:
                 outline.append(i)
-            elif i in dd and 'mn-t' in dd[i]:
+            elif i in dd and "mn-t" in dd[i]:
                 # flatten multiple pronunciations into a single list
-                prons = [rec.split("/") for rec in dd[i]['mn-t']]
+                prons = [rec.split("/") for rec in dd[i]["mn-t"]]
                 prons = [item for sublist in prons for item in sublist]
                 prons = sorted(set(prons))
                 outline.append(rubify(i, "<br/>".join(prons)))
@@ -80,15 +96,15 @@ if args.format == 'html':
     print("</body></html>")
 
 # output replace text with peng'im
-elif args.format=="replace":
+elif args.format == "replace":
     for line in args.input:
         outline = []
         line = line.rstrip()
         for i in line:
             if i in [str(i) for i in range(10)]:
                 outline.append(i)
-            elif i in dd and 'mn-t' in dd[i]:
-                    outline.append("/".join(dd[i]['mn-t']))
+            elif i in dd and "mn-t" in dd[i]:
+                outline.append("/".join(dd[i]["mn-t"]))
             else:
                 outline.append(i)
         print("".join(outline))
@@ -102,9 +118,9 @@ else:
             if i in [str(i) for i in range(10)]:
                 # Avoid numerals
                 outline.append(i)
-            elif i in dd and 'mn-t' in dd[i]:
+            elif i in dd and "mn-t" in dd[i]:
                 # characters with Pengim, if multiple join with /
-                outline.append("/".join(dd[i]['mn-t']) + " ")
+                outline.append("/".join(dd[i]["mn-t"]) + " ")
             elif i in puncdict:
                 # CJK punctuation to western
                 outline.append(puncdict[i] + " ")
